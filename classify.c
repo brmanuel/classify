5b1a9c748d8e6a5711e63c3c1a4614bdfc3da23b
emfuthal <ewaldboss@hotmail.com>
2017-02-05 18:54:53 +0100

    functionality wholly implemented, still some bugs

--- classify.c
+++ classify.c
@@ -14,6 +14,14 @@
 
 #endif
 
+// names of the files containing raw data:
+// spam holds words and quantities of words that occurred in spam mail
+// honest holds words and quantities of words that occurred in honest mail
+// pforspam holds two doubles. the first one holds #total mails, the second one #spam mails -> P[S] = #spam mails / #total mails.
+static char* spamdata = "spmdt.txt";
+static char* honestdata = "hnstdt.txt";
+static char* pforspamdata = "pfors.txt";
+
 
 // helpers for storing contents (words) in a tree (tree specified in avl.h)
 
@@ -30,6 +38,13 @@ struct node* mktree(char* vals[], int length){
     insert(root, vals[i], 1);
   }
   return root;
+}
+
+struct node* mktreefromMail(char* filename){
+  int txtlength;
+  char** words = parsetxt(filename, &txtlength);
+  struct node* tree = mktree(words, txtlength);
+  return tree;
 }
 
 struct node* mktreefromFile(char* filename){
@@ -75,18 +90,17 @@ void writetree2file(char* filename, struct node* root){
 
 // Computes the probability ratio p(S|D)/p(~S|D). if result > 1 it is likely that the email is Spam
 // parameters:
-// in: emailname is the name of the .txt file holding the email to classify
-// in: spamdata is the name of the .txt file holding prior observations of words that appear in spam plus their quantities
+// in: mail is a tree  holding info about the email to classify
+// in: spamdata is a tree  holding info about the prior observations of words that appear in spam plus their quantities
 // in: honestdata is the equivalent for honest mails
-// in: probforspam is again a .txt filename in which a single number is saved that reflects the current estimate of p(S). this will change over time.
+// in: pspam estimates p(S). this will change over time.
 // out: the probability ratio
 
-double probability(char* emailname, struct node* spam, struct node* honest, double pspam){ 
+double probability(struct node* mail, struct node* spam, struct node* honest, double pspam){ 
   double PS, PnS, PDgS, PDgnS; // P[Spam], P[~Spam], P[Email|Spam], P[Email|~Spam]
   PS = pspam;
   PnS = 1.-PS;
 
-  struct node* mail = mktreefromFile(emailname);
   int totalwrds = totalwords(spam);
   PDgS = 1.;
   inline void compprob(struct node* a){
@@ -104,30 +118,86 @@ double probability(char* emailname, struct node* spam, struct node* honest, doub
   return (PS/PnS) * (PDgS/PDgnS);
 }
 
-void classify(char* emailname){
-  char* spam = "spmdt.txt";
-  char* honest = "hnstdt.txt";
-  char* pforspam = "pfors.txt";
-  struct node* sp = mktreefromFile(spam);
-  struct node* hn = mktreefromFile(honest);
-  FILE* fp = fopen(pforspam, "r
-  
+void updateDatabase(double dec, int totalmail, int spammail, struct node* spam, struct node* honest, struct node* mail){
+  if (dec > 1){
+    spammail += 1;
+    mergetrees(mail, spam);
+    writetree2file(spamdata, mail);
+  } else {
+    mergetrees(mail, honest);
+    writetree2file(honestdata, mail);
+  }
+  totalmail += 1;
+  FILE* p = fopen(pforspamdata, "w");
+  if (!p){
+    printf("couldn't write back to file pspam.txt");
+    return;
+  }
+  fprintf(p, "%d %d", totalmail, spammail);
+  fclose(p);
+}
+
+int classify(char* emailname){
+  struct node* mail = mktreefromMail(emailname);
+  struct node* sp = mktreefromFile(spamdata);
+  if (!sp) {
+    printf("Spam training required. Spam datasheet empty");
+    return -1;
+  }
+  struct node* hn = mktreefromFile(honestdata);
+  if (!hn) {
+    printf("Honest training required. Honest datasheet empty");
+    return -1;
+  }
+  double ps;
+  int totalmails, spammails;
+  FILE* fp = fopen(pforspamdata, "r");
+  if (!fp) ps = 0.5;
+  else if (fscanf(fp, "%d %d", &totalmails, &spammails) < 2) ps = 0.5;
+  else ps = ((double) spammails)/((double)totalmails);
+
+  double decision = probability(mail, sp, hn, ps);
+
+  updateDatabase(decision, totalmails, spammails, sp, hn, mail);
+
+  if (decision > 1.) return 1;
+  return 0;  
 }
 
 void training(char* emailname, int spam){
-  char* spam = "spmdt.txt";
-  char* honest = "hnstdt.txt";
-  char* pforspam = "pfors.txt";
-  
+  FILE* pforspam = fopen(pforspamdata, "w+");
+  int totalmail, spammail;
+  if (!fscanf(pforspam, "%d %d", &totalmail, &spammail)){
+    totalmail = 0;
+    spammail = 0;
+  }
+  struct node* mail = mktreefromMail(emailname);
+  if (spam) {
+    struct node* sp = mktreefromFile(spamdata);
+    if (sp) {
+      mergetrees(mail, sp);
+    }
+    writetree2file(spamdata, mail);
+    spammail += 1.;
+  } else {
+    struct node* hn = mktreefromFile(honestdata);
+    if (hn) {
+      mergetrees(mail, hn);
+    }
+    writetree2file(honestdata, mail);
+  }
+  totalmail += 1;
+  fprintf(pforspam, "%d %d", totalmail, spammail);  
 } 
 
 int main(){
-  int txtlength;
+  /*int txtlength;
   char** words = parsetxt("exp.txt", &txtlength);
   struct node* tree = mktree(words, txtlength);
   writetree2file("exp2.txt", tree);
   struct node* tree2 = mktreefromFile("exp2.txt");
   mergetrees(tree, tree2);
-  printtree(tree);
+  printtree(tree);*/
+  training("honest1.txt", 0);
   return 0;
 }
